31d05608a97c4a7388065d55c32a85c14eb06a2b
NghiepPham-96 <99112092+NghiepPham-96@users.noreply.github.com>
2023-12-09 02:18:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ echo 'eval "$(starship init bash)"' >> /etc/profile.d/starship.sh
 chmod 777 /etc/profile.d/starship.sh
 . /etc/profile.d/starship.sh
 # [bash alias]
-echo "# More alias to upgrade 'ls'" >> /etc/profile.d/alias.sh
+echo "# More alias to upgrade 'ls'" > /etc/profile.d/alias.sh
 echo "alias ll='exa -abghHlFiS --icons --group-directories-first --git'" >> /etc/profile.d/alias.sh
 echo "alias l='exa --icons -F -H --group-directories-first --git -1'" >> /etc/profile.d/alias.sh
 echo "alias ltree='exa --long --tree --icons --group-directories-first'" >> /etc/profile.d/alias.sh
